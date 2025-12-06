1d033c74f1862815d291acef947403c76c823cbb
Junyeong Heo <hhjjyy1122@gmail.com>
2025-12-06 14:42:50 +0900

    chore: vitest set-up update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,14 @@
-import path from "path";
+/// <reference types="vitest" />
+
 import { configDefaults, defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  esbuild: {
+    target: "es2022",
+  },
+  plugins: [tsconfigPaths()],
   test: {
     exclude: [...configDefaults.exclude, ".local/**", "./test/ch2/**"],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   },
 });
